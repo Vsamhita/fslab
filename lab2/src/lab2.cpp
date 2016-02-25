b0ae5393e743fd2e3a269cb157ab71c0b622a9fa
Vsamhita <vsamhita10@gmail.com>
2016-02-25 14:18:35 +0530

    first commit of program2

--- lab2/src/lab2.cpp
+++ lab2/src/lab2.cpp
@@ -1,5 +1,5 @@
 //============================================================================
-// Name        : lab2.cpp
+// Name        : lab2a.cpp
 // Author      : 
 // Version     :
 // Copyright   : Your copyright notice
@@ -7,9 +7,66 @@
 //============================================================================
 
 #include <iostream>
+#include<sstream>
 using namespace std;
+class student{
+	string usn;
+	string name;
+	string branch;
+	int semester;
+
+public:
+	void readfromconsole() {
+		cout<<"enter the usn"<<endl;
+		cin>>usn;
+		cin.clear();
+		cin.ignore(255,'\n');
+		cout<<"enter the name"<<endl;
+		getline(cin,name);
+		cout<<"enter the branch"<<endl;
+		cin>>branch;
+		cout<<"enter the semester"<<endl;
+		cin>>semester;
+	}
+	void showstudent()
+	{
+		cout<<usn<<endl<<name<<endl<< branch<<endl<< semester<<endl;
+	}
+	void pack()
+	{
+		string buffer;
+		string sem1;
+		stringstream out;
+		out<<semester;
+		sem1=out.str();
+		string temp;
+		int j,len;
+		temp=usn+'|'+name+'|'+branch+'|'+sem1;
+		len=temp.size();
+		buffer=temp;
+		for(j=len+1;j<100;j++)
+		{
+			temp+='$';
+		}
+	}
+};
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	int choice;
+	student s;
+	while(1)
+	{
+		cout<<"1.insert"<<endl<<"2.search"<<endl<<" 3.delete"<<endl<<" 4.modify"<<endl;
+		cout<<"enter choice";
+		cin>>choice;
+		switch(choice)
+		{
+		case 1:s.readfromconsole();
+		       s.showstudent();
+		       s.pack();
+		       break;
+
+		}
+	}
 	return 0;
 }
